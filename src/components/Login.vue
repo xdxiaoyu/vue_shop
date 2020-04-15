03d47a770c3d8396597b029976ba8cfac1768660
丁星星 <1693992774@qq.com>
2020-04-15 23:55:59 +0800

    安装less，删除无用路由模块，新建登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scope>
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+
+</style>
