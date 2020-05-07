98b6a5d8ff40f07a4c8219fa45600a7effcf0c67
丁星星 <1693992774@qq.com>
2020-05-07 23:40:54 +0800

    完成了登录

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>Home组件</div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -18,4 +18,16 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行 next('/login')  强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
